71bc0fc6dfdd185421de63e379011d5325a49fe5
Takayoshi Watanabe <takayoshi.watanabe@asvin.co.jp>
2023-05-20 14:45:37 +0900

    add: サイト名対策

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -3,9 +3,9 @@
     <div class="px-3 py-2 bg-primary text-white">
       <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <div class="col-5 me-lg-auto text-start fs-4">
+          <h1 class="col-5 me-lg-auto my-0 text-start fs-4">
             みどり市ポータル
-          </div>
+          </h1>
           <ul class="nav col-7 col-lg-auto my-2 justify-content-center my-md-0 text-small">
             <li>
               <a href="#" class="nav-link text-white">
