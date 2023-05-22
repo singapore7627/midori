bd20c02d13d7f9798b3fcb978375910086126cb5
Takayoshi Watanabe <takayoshi.watanabe@asvin.co.jp>
2023-05-22 09:16:38 +0900

    add: グルメ紹介ページ

---
+++ src/views/MealView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <PageHeader />
+    <Meal />
+    <PageFooter />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Meal from '@/components/Meal.vue';
+import PageHeader from '@/components/PageHeader.vue';
+import PageFooter from '@/components/PageFooter.vue';
+import { useHead } from 'unhead'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    Meal,
+    PageHeader,
+    PageFooter,
+  },
+  mounted() {
+    useHead({
+      title: 'みどり市ポータル | グルメ紹介',
+      meta: [
+        {
+          name: 'description',
+          content: 'みどり市のグルメを紹介します。',
+        },
+        {
+          name: 'keywords',
+          content: 'みどり市, 笠懸町, 大間々町, 東村, ごみカレンダー, ごみ, 観光, グルメ, ランチ',
+        },
+      ],
+    });
+  }
+});
+</script>
