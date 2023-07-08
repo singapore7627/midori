089d26116d4bfbc998d1361dd5a5eb6fa4620465
Takayoshi Watanabe <takayoshi.watanabe@asvin.co.jp>
2023-07-08 12:37:38 +0900

    refactor: 不要コード削除

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,14 +16,6 @@ const routes: Array<RouteRecordRaw> = [
     component: TrashCalendarView,
   },
   {
-    path: '/meal-list',
-    name: 'meal-list',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
     path: '/meals',
     name: 'meals',
     component: MealsView,
